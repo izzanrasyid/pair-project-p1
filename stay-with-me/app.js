e243e8d9a9d2e80a44921009cf50649d0d751540
fauzanhilmy <fauzanhilmy20@gmail.com>
2021-03-28 22:05:53 +0700

    Fauzan Hilmy & Izzan Rasyid

--- stay-with-me/app.js
+++ stay-with-me/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const route = require('./routes/index')
 const session = require('express-session')
 const app = express()
-const port = 3000
+const port = process.env.port || 3000
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
